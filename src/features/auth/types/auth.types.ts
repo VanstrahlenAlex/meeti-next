70437524f5af3f6dbbb13cb80e04f59ef627482b
Alexander Van strahlen <75139319+VanstrahlenAlex@users.noreply.github.com>
2026-04-22 18:14:58 -0500

    feat: implement authentication infrastructure with BetterAuth integration and repository pattern

---
+++ src/features/auth/types/auth.types.ts
@@ -0,0 +1,4 @@
+import { auth } from "@/src/lib/auth";
+
+
+export type User = typeof auth.$Infer.Session.user | null;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,3 +15,5 @@ export const auth = betterAuth({
 	},
 	plugins: [nextCookies()]
 })
+
+
